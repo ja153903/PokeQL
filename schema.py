1af317c03fde7bb8ed3c1f45b8e85cefce14adcf
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-21 22:10:31 -0500

    updating stuff

--- schema.py
+++ schema.py
@@ -1,20 +1,29 @@
-import strawberry
 import typing
+import strawberry
 
 from datasource import PokemonDataSource
 
 
 @strawberry.type
 class Berry:
-    id: int
+    id: strawberry.ID
     name: str
     growth_time: int
     max_harvest: int
+    natural_gift_power: int
+    smoothness: int
+    soil_dryness: int
     size: int
 
 
-def hello() -> str:
-    return "Hello! This is the pokeql GraphQL API"
+@strawberry.type
+class Pokemon:
+    id: strawberry.ID
+    name: str
+    base_experience: int
+    height: int
+    order: int
+    weight: int
 
 
 async def get_berry_by_name(name: str) -> typing.Optional[Berry]:
@@ -23,18 +32,38 @@ async def get_berry_by_name(name: str) -> typing.Optional[Berry]:
         return None
 
     return Berry(
-        id=data.get('id'),
-        name=data.get('name'),
-        growth_time=data.get('growth_time'),
-        max_harvest=data.get('max_harvest'),
-        size=data.get('size')
+        id=data.get("id"),
+        name=data.get("name"),
+        growth_time=data.get("growth_time"),
+        max_harvest=data.get("max_harvest"),
+        natural_gift_power=data.get("natural_gift_power"),
+        smoothness=data.get("smoothness"),
+        soil_dryness=data.get("soil_dryness"),
+        size=data.get("size"),
+    )
+
+
+async def get_pokemon_by_name(name: str) -> typing.Optional[Pokemon]:
+    data = await PokemonDataSource.get("pokemon", name)
+    if not data:
+        return None
+
+    return Pokemon(
+        id=data.get("id"),
+        name=data.get("name"),
+        base_experience=data.get("base_experience"),
+        height=data.get("height"),
+        order=data.get("order"),
+        weight=data.get("weight"),
     )
 
 
 @strawberry.type
 class Query:
-    hello: str = strawberry.field(resolver=hello)
     berry_by_name: typing.Optional[Berry] = strawberry.field(resolver=get_berry_by_name)
+    pokemon_by_name: typing.Optional[Pokemon] = strawberry.field(
+        resolver=get_pokemon_by_name
+    )
 
 
 schema = strawberry.Schema(query=Query)
