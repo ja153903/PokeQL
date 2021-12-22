ba73ab7bfa9a235f4e318edab609a6891a326ba9
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-12-21 22:52:03 -0500

    adding some docstring

--- schema.py
+++ schema.py
@@ -5,6 +5,17 @@ from datasource import PokemonDataSource
 
 
 @strawberry.type
+class BerryFlavor:
+    name: str
+
+
+@strawberry.type
+class BerryFlavorMap:
+    potency: int
+    flavor: BerryFlavor
+
+
+@strawberry.type
 class Berry:
     id: strawberry.ID
     name: str
@@ -14,6 +25,7 @@ class Berry:
     smoothness: int
     soil_dryness: int
     size: int
+    flavors: typing.List[BerryFlavorMap]
 
 
 @strawberry.type
@@ -26,10 +38,44 @@ class Pokemon:
     weight: int
 
 
+def format_berry_flavor(
+    flavors: typing.List[typing.Dict[str, typing.Any]]
+) -> typing.List[BerryFlavorMap]:
+    """
+    format_berry_flavor formats the list of flavors that get from the PokeAPI
+    when we query for berries.
+
+    :param flavors: a list of dictionaries containing data about the berry flavor
+    :return: A list of BerryFlavorMap objects
+    """
+    result = []
+
+    for flavor_dict in flavors:
+        potency = flavor_dict.get("potency")
+        berry_flavor_dict = flavor_dict.get("flavor")
+        berry_flavor_name = berry_flavor_dict.get("name")
+
+        berry_flavor = BerryFlavor(name=berry_flavor_name)
+        berry_flavor_map = BerryFlavorMap(potency=potency, flavor=berry_flavor)
+
+        result.append(berry_flavor_map)
+
+    return result
+
+
 async def get_berry_by_name(name: str) -> typing.Optional[Berry]:
+    """
+    get_berry_by_name creates a Berry object for querying based on
+    the berry's name.
+
+    :param name: a string denoting the name of a berry
+    :return: None | a Berry object
+    """
     data = await PokemonDataSource.get("berry", name)
     if not data:
         return None
+
+    flavors = format_berry_flavor(data.get("flavors", []))
 
     return Berry(
         id=data.get("id"),
@@ -40,10 +86,18 @@ async def get_berry_by_name(name: str) -> typing.Optional[Berry]:
         smoothness=data.get("smoothness"),
         soil_dryness=data.get("soil_dryness"),
         size=data.get("size"),
+        flavors=flavors,
     )
 
 
 async def get_pokemon_by_name(name: str) -> typing.Optional[Pokemon]:
+    """
+    get_pokemon_by_name creates a Pokemon object for querying based on
+    the pokemon's name
+
+    :param name: a string denoting the name of a pokemon
+    :return: None | a Pokemon object
+    """
     data = await PokemonDataSource.get("pokemon", name)
     if not data:
         return None
